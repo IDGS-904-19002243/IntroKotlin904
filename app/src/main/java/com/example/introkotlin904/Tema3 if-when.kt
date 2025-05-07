15894e4027b2da26170678395ced649529487338
Jose Ruben Pedroza Moctezuma <19002243@alumnos.utleon.edu.mx>
2025-05-06 23:08:11 -0600

    Commit #2: Archivo del Tema3

--- app/src/main/java/com/example/introkotlin904/Tema3 if-when.kt
+++ app/src/main/java/com/example/introkotlin904/Tema3 if-when.kt
@@ -18,7 +18,7 @@ fun main(){
 
     print("--------------------------------------------------------------------------------------")
     print("Ingrese el sueldo del empleado: ")
-    val sueldo = readln().toDouble()
+    var sueldo = readln().toDouble()
     if (sueldo > 3000) {
         println("Debe pagar impuestos")
     }
